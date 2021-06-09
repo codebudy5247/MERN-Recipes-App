94cf2f0bc48312f448db5feb3f571306fdafd45f
codebudy5247 <a.shekhar5247@gmail.com>
2021-06-10 01:03:12 +0530

    :hammer:

--- api/server.js
+++ api/server.js
@@ -37,6 +37,6 @@ const PORT = process.env.PORT || 5001;
 app.listen(
     PORT,
     console.log(
-        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+        `Server running on port ${PORT}`.yellow.bold
     )
 );
