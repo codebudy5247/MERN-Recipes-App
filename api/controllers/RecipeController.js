00ba87f4662bf2a0f27edb9a2b9a0be6780f1226
codebudy5247 <a.shekhar5247@gmail.com>
2021-06-10 02:32:15 +0530

    modify recipe controller

--- api/controllers/RecipeController.js
+++ api/controllers/RecipeController.js
@@ -1,6 +1,6 @@
 const Recipe = require('../models/recipeModel');
 
-
+//Create A New Recipe
 exports.addRecipe = async (req, res) => {
 
     try {
@@ -19,12 +19,27 @@ exports.addRecipe = async (req, res) => {
         res.status(409).json({ message: error.message });
     }
 }
-exports.getRecipe = async (req, res) => {
-    try {
-        const recipe = await Recipe.find().sort({ createdAt: -1 });
 
-        res.status(200).json(recipe);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
+//Get All Recipes
+exports.getRecipe = async (req, res) => {
+    const pageSize = 10;
+  const page = Number(req.query.pageNumber) || 1;
+  const keyword = req.query.keyword
+    ? {
+        name: {
+          $regex: req.query.keyword,
+          $options: "i",
+        },
+      }
+    : {};
+  const count = await Recipe.countDocuments({ ...keyword });
+  try {
+    const posts = await Recipe.find({ ...keyword })
+      .limit(pageSize)
+      .skip(pageSize * (page - 1));
+
+    res.status(200).json({ posts, page, pages: Math.ceil(count / pageSize) });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 }
